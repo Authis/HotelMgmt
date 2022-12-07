893676104ae37846c9b1bbd084810fcc60ff9e51
authis.sundar <authis.sundar@gmail.com>
2022-12-07 18:52:18 +0530

    Adding correctted files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import ReactDOM from ‘react-dom’;
-import React from ‘react’;
+import ReactDOM from "react-dom";
+import React from "react";
+import { promises as fsPromises } from 'fs';
 const App = () => {
  return <h1>This is my React app!</h1>;
  }
-ReactDOM.render(<App />, document.getElementById(‘app’));
+ReactDOM.render(<App />, document.getElementById("app"));
